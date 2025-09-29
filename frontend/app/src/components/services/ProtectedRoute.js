3abb92a1d8c5aaa900274a5fc70aa4c904200b40
Padmakumar18 <padmakumar41759@gmail.com>
2025-09-29 20:41:51 +0530

    😁

---
+++ frontend/app/src/components/services/ProtectedRoute.js
@@ -0,0 +1,13 @@
+// import { Navigate } from "react-router-dom";
+// import { handleLogin } from "./services/AuthService";
+
+// const ProtectedRoute = ({ children }) => {
+//   const email = localStorage.getItem("hsp-email-id");
+//   const password = localStorage.getItem("hsp-password");
+
+//   if (!email || !password) return <Navigate to="/auth" replace />;
+
+//   return children;
+// };
+
+// export default ProtectedRoute;
